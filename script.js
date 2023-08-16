73e7845d888b3002c982559dd76bbbf0842010ec
Gaurav <gaurav556pant@gmail.com>
2023-08-16 18:59:01 +0530

    updating on a pdf downloader file

---
+++ script.js
@@ -0,0 +1,19 @@
+// Function to handle button click event
+document.getElementById("downloadButton").addEventListener("click", function () {
+    // URL of the PDF file
+    var pdfUrl = "Gaurav Resume.pdf";
+
+    // Create an anchor element to initiate the download
+    var anchor = document.createElement("a");
+    anchor.href = pdfUrl;
+    anchor.download = "Gaurav Resume.pdf"; // Change the file name if needed
+    anchor.target = "_blank"; // Open in a new tab
+    anchor.style.display = "none";
+
+    // Append the anchor element to the body and trigger the click event
+    document.body.appendChild(anchor);
+    anchor.click();
+
+    // Clean up: remove the anchor element from the body
+    document.body.removeChild(anchor);
+});
